a6fc9fd8e945224907e6068ab0837aafe90abe8b
Mbra Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2024-05-22 22:43:45 +0000

    to remove

--- components/MoviesList.tsx
+++ components/MoviesList.tsx
@@ -1,59 +1,14 @@
-import {
-  View,
-  Text,
-  StyleSheet,
-  ActivityIndicator,
-  FlatList,
-} from "react-native";
-import React, { useState, useEffect } from "react";
-import fetchApiRequest from "../api/request";
-import MovieItem from "./MovieItem";
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
 interface Props {
   title: string;
-  endPointUrl: string;
 }
 
-const MoviesList = ({ title, endPointUrl }: Props) => {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState<Boolean>(false);
-  useEffect(() => {
-    async function fetchMovies() {
-      try {
-        setIsLoading(true);
-        const resData = await fetchApiRequest(endPointUrl);
-
-        setMovies(resData.results);
-        setIsLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-    fetchMovies();
-  }, [endPointUrl]);
-
-  if (isLoading) {
-    return (
-      <View style={styles.activityIndicator}>
-        <ActivityIndicator />
-        <Text style={{ color: "white" }}>Recuperation des films</Text>
-      </View>
-    );
-  }
-
+const MoviesList = ({ title }: Props) => {
   return (
     <View style={styles.root}>
       <Text style={styles.title}>{title}</Text>
-      <FlatList
-        horizontal
-        data={movies}
-        renderItem={({ item }: { item: any }) => {
-          return <MovieItem movie={item} />;
-        }}
-        keyExtractor={(item) => item.id}
-        showsHorizontalScrollIndicator={false}
-      />
     </View>
   );
 };
@@ -66,13 +21,6 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 18,
     color: "white",
-  },
-
-  activityIndicator: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 10,
   },
 });
 
--- components/ui/Banner.tsx
+++ components/ui/Banner.tsx
@@ -1,35 +1,10 @@
-import { View, Text, StyleSheet, ImageBackground } from "react-native";
-import React, { useEffect, useState } from "react";
-import fetchApiRequest from "../../api/request";
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const Banner = ({ endPointUrl }: { endPointUrl: string }) => {
-  const [movie, setMovie] = useState<any>();
-
-  useEffect(() => {
-    async function fetchBanner() {
-      try {
-        const resData = await fetchApiRequest(endPointUrl);
-        setMovie(
-          resData.results[
-            Math.floor(Math.random() * resData.results.length - 1)
-          ]
-        );
-      } catch (error) {
-        console.log(`Erreur banner ${error}`);
-      }
-    }
-
-    fetchBanner();
-  }, [endPointUrl]);
-
+const Banner = () => {
   return (
     <View style={styles.root}>
-      <ImageBackground
-        source={{
-          uri: `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`,
-        }}
-        style={styles.banner}
-      />
+      <Text>Banner</Text>
     </View>
   );
 };
@@ -37,12 +12,7 @@ const Banner = ({ endPointUrl }: { endPointUrl: string }) => {
 const styles = StyleSheet.create({
   root: {
     height: 350,
-  },
-  banner: {
-    height: "100%",
-    width: "100%",
-    borderRadius: 5,
-    overflow: "hidden",
+    backgroundColor: "red",
   },
 });
 
