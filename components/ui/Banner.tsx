4f12f847ab1072cdfb303ce77da5c49eca43cdcf
Mbra Oliver <96344566+Mbra-Oliver@users.noreply.github.com>
2024-05-25 04:14:50 +0000

    Banner script

--- components/ui/Banner.tsx
+++ components/ui/Banner.tsx
@@ -8,10 +8,21 @@ import {
 import React, { useEffect, useState } from "react";
 import fetchApiRequest from "../../api/request";
 import LoadingIndicator from "./LoadingIndicator";
+import { Feather, Entypo } from "@expo/vector-icons";
+import { IMovie } from "../../interfaces/IMovie";
+
+const MovieGenderItem = () => {
+  return (
+    <View style={styles.genderItem}>
+      <View style={styles.genderCircle} />
+      <Text style={styles.genderText}>Enigme</Text>
+    </View>
+  );
+};
 
 const Banner = ({ endPointUrl }: { endPointUrl: string }) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [movie, setMovie] = useState<any>();
+  const [movie, setMovie] = useState<IMovie>();
 
   useEffect(() => {
     async function fetchMovies() {
@@ -42,6 +53,8 @@ const Banner = ({ endPointUrl }: { endPointUrl: string }) => {
     return;
   }
   const moviePictureUrl = `https://image.tmdb.org/t/p/w500/${movie?.poster_path}`;
+
+  console.log(movie);
   return (
     <View style={styles.root}>
       <ImageBackground
@@ -50,13 +63,29 @@ const Banner = ({ endPointUrl }: { endPointUrl: string }) => {
         }}
         style={styles.image}
         fadeDuration={100}
-      />
+      >
+        <View style={styles.bottomBtns}>
+          <View style={{ flexDirection: "row", gap: 10 }}>
+            <MovieGenderItem />
+            <MovieGenderItem />
+            <MovieGenderItem />
+          </View>
 
-      <View style={{ backgroundColor: "pink" }}>
-        <Pressable>
-          <Text>Lecture</Text>
-        </Pressable>
-      </View>
+          <View style={styles.btnsContainer}>
+            <Pressable style={styles.coverPlayBtn}>
+              <Entypo name="controller-play" size={24} color="black" />
+              <Text>Jouer</Text>
+            </Pressable>
+
+            <Pressable style={styles.coverPlayBtnOutline}>
+              <Feather name="plus" size={24} color="white" />
+              <Text style={{ color: "white", fontWeight: "bold" }}>
+                Ma liste
+              </Text>
+            </Pressable>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
@@ -72,6 +101,55 @@ const styles = StyleSheet.create({
     height: "100%",
     width: "100%",
     objectFit: "cover",
+  },
+
+  bottomBtns: {
+    flex: 1,
+    justifyContent: "flex-end",
+    gap: 20,
+    padding: 20,
+  },
+
+  btnsContainer: {
+    flexDirection: "row",
+    paddingHorizontal: 10,
+    justifyContent: "center",
+    gap: 10,
+  },
+  genderCircle: {
+    width: 10,
+    height: 10,
+    backgroundColor: "white",
+    borderRadius: 30,
+  },
+
+  genderItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 5,
+  },
+
+  genderText: {
+    fontWeight: "bold",
+    color: "white",
+  },
+  coverPlayBtn: {
+    backgroundColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5,
+    borderRadius: 2,
+    width: "50%",
+  },
+  coverPlayBtnOutline: {
+    backgroundColor: "gray",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 5,
+    borderRadius: 2,
+    width: "50%",
   },
 });
 
